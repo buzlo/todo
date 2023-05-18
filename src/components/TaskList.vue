3cec28e23527d51621f311efd8df1c3c5bf4fee4
Dmitry <dm.buzaev@mail.ru>
2023-05-18 14:27:45 +0300

    initial vue commit

---
+++ src/components/TaskList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+createSwitchStorageBtn(isLocalStorage);
+const owner = "me",
+  todoItemList = await getTodoList(owner);
+createTodoApp(document.getElementById("todo-app"), {
+  title: "Мои дела",
+  owner,
+  todoItemList,
+  onCreateFormSubmit: createTodoItem,
+  onDoneBtnCLick: switchTodoItemDoneStatus,
+  onDeleteBtnClick: deleteTodoItem,
+});
+</script>
+
+<template></template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
